317a649edb9824c84580ba8511f98b3f7fc76910
matija.mazi@gmail.com <matija.mazi@gmail.com>
2013-10-12 21:21:34 +0200

    Folding builder proof of concept (not a very good folding builder though)

--- src/org/intellij/plugins/ceylon/lang/CeylonFoldingBuilder.java
+++ src/org/intellij/plugins/ceylon/lang/CeylonFoldingBuilder.java
@@ -5,26 +5,33 @@ import com.intellij.lang.folding.FoldingBuilderEx;
 import com.intellij.lang.folding.FoldingDescriptor;
 import com.intellij.openapi.editor.Document;
 import com.intellij.psi.PsiElement;
-import org.intellij.plugins.ceylon.psi.CeylonBlock;
+import com.intellij.psi.tree.IElementType;
+
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
+import static org.intellij.plugins.ceylon.psi.CeylonTypes.*;
+
 public class CeylonFoldingBuilder extends FoldingBuilderEx {
+    private static final List<IElementType> blockTypes = Arrays.asList(
+            TYPE, ABSTRACTED_TYPE, ANY_CLASS, ANY_INTERFACE, ANY_METHOD, FOR_STATEMENT, SWITCH_STATEMENT, IF_STATEMENT, IMPORT_LIST);
+
     @NotNull
     @Override
     public FoldingDescriptor[] buildFoldRegions(@NotNull PsiElement root, @NotNull Document document, boolean quick) {
         List<FoldingDescriptor> descriptors = new ArrayList<FoldingDescriptor>();
 
-//        appendDescriptors(root, descriptors);
+        appendDescriptors(root, descriptors);
 
         return descriptors.toArray(new FoldingDescriptor[descriptors.size()]);
     }
 
     private void appendDescriptors(PsiElement root, List<FoldingDescriptor> descriptors) {
-        if (root instanceof CeylonBlock) {
+        if (blockTypes.contains(root.getNode().getElementType())) {
             descriptors.add(new FoldingDescriptor(root.getNode(), root.getTextRange()));
         }
 
