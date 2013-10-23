86b15d2abd3f9ea78bcf3bac5959d39f00fb553e
matija.mazi@gmail.com <matija.mazi@gmail.com>
2013-10-23 11:37:24 +0200

    Run Configuration: keep logic (first top-level method), refactor a bit (reuse code etc.)

--- src/org/intellij/plugins/ceylon/runner/CeylonRunConfigurationProducer.java
+++ src/org/intellij/plugins/ceylon/runner/CeylonRunConfigurationProducer.java
@@ -1,5 +1,6 @@
 package org.intellij.plugins.ceylon.runner;
 
+import com.intellij.execution.Location;
 import com.intellij.execution.actions.ConfigurationContext;
 import com.intellij.execution.actions.RunConfigurationProducer;
 import com.intellij.execution.configurations.ConfigurationType;
@@ -8,10 +9,12 @@ import com.intellij.lang.ASTNode;
 import com.intellij.openapi.extensions.Extensions;
 import com.intellij.openapi.util.Ref;
 import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiFile;
 import com.intellij.util.containers.ContainerUtil;
 import org.intellij.plugins.ceylon.psi.CeylonFile;
 import org.intellij.plugins.ceylon.psi.CeylonPsi;
 import org.intellij.plugins.ceylon.psi.CeylonTypes;
+import org.jetbrains.annotations.Nullable;
 
 /**
  * TODO
@@ -24,58 +27,47 @@ public class CeylonRunConfigurationProducer extends RunConfigurationProducer<Run
 
     @Override
     protected boolean setupConfigurationFromContext(RunConfiguration configuration, ConfigurationContext context, Ref<PsiElement> sourceElement) {
-        // TODO I think we could create a run configuration from the method under the caret (if sourceElement is an identifier and has a parent of type AnyMethodPsi)
-        if (!(sourceElement.get() instanceof CeylonFile)) {
-            return false;
-        }
-        CeylonFile file = (CeylonFile) sourceElement.get();
-        CeylonRunConfiguration runConfig = (CeylonRunConfiguration) configuration;
+        PsiFile file = sourceElement.get().getContainingFile();
+        if (file instanceof CeylonFile) {
+            CeylonRunConfiguration runConfig = (CeylonRunConfiguration) configuration;
 
+            final ASTNode identifier = getMethodNode(context.getLocation());
 
-        CeylonPsi.DeclarationPsi topLevelDeclaration = file.findChildByClass(CeylonPsi.DeclarationPsi.class);
-        if (topLevelDeclaration != null) {
-            ASTNode topLevelMethod = topLevelDeclaration.getNode().findChildByType(CeylonTypes.ANY_METHOD);
-
-            if (topLevelMethod != null) {
-                ASTNode identifier = topLevelMethod.findChildByType(CeylonTypes.IDENTIFIER);
-
-                if (identifier != null) {
-                    runConfig.setFilePath(file.getVirtualFile().getCanonicalPath());
-                    runConfig.setTopLevelName(identifier.getText());
-                    runConfig.setName(identifier.getText() + "() in " + file.getName());
-                }
+            if (identifier != null) {
+                runConfig.setFilePath(file.getVirtualFile().getCanonicalPath());
+                runConfig.setTopLevelName(identifier.getText());
+                runConfig.setName(identifier.getText() + "() in " + file.getName());
+                return true;
             }
         }
-
-        return runConfig.getName() != null;
+        return false;
     }
 
     @Override
     public boolean isConfigurationFromContext(RunConfiguration configuration, ConfigurationContext context) {
-        if (!(configuration instanceof CeylonRunConfiguration)) {
-            return false;
+        ASTNode identifier = null;
+        if ((configuration instanceof CeylonRunConfiguration)) {
+            identifier = getMethodNode(context.getLocation());
         }
 
+        return identifier != null && identifier.getText().equals(((CeylonRunConfiguration) configuration).getTopLevelName());
+    }
+
+    private ASTNode getMethodNode(Location contextLocation) {
+        ASTNode identifier = null;
         // TODO be more accurate than containing file (e.g. check if we are inside the same toplevel method)
-        if (!(context.getLocation().getPsiElement().getContainingFile() instanceof CeylonFile)) {
-            return false;
-        }
+        if ((contextLocation != null && contextLocation.getPsiElement().getContainingFile() instanceof CeylonFile)) {
+            CeylonFile file = (CeylonFile) contextLocation.getPsiElement().getContainingFile();
 
-        CeylonFile file = (CeylonFile) context.getLocation().getPsiElement().getContainingFile();
+            CeylonPsi.DeclarationPsi topLevelDeclaration = file.findChildByClass(CeylonPsi.DeclarationPsi.class);
+            if (topLevelDeclaration != null) {
+                ASTNode topLevelMethod = topLevelDeclaration.getNode().findChildByType(CeylonTypes.ANY_METHOD);
 
-        CeylonPsi.DeclarationPsi topLevelDeclaration = file.findChildByClass(CeylonPsi.DeclarationPsi.class);
-        if (topLevelDeclaration != null) {
-            ASTNode topLevelMethod = topLevelDeclaration.getNode().findChildByType(CeylonTypes.ANY_METHOD);
-
-            if (topLevelMethod != null) {
-                ASTNode identifier = topLevelMethod.findChildByType(CeylonTypes.IDENTIFIER);
-
-                if (identifier != null) {
-                    return identifier.getText().equals(((CeylonRunConfiguration) configuration).getTopLevelName());
+                if (topLevelMethod != null) {
+                    identifier = topLevelMethod.findChildByType(CeylonTypes.IDENTIFIER);
                 }
             }
         }
-
-        return false;
+        return identifier;
     }
 }
