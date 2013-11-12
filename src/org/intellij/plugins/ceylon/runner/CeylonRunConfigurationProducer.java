8248c88e9306953ab7482a8faf55661b966b1c3c
matija.mazi@gmail.com <matija.mazi@gmail.com>
2013-11-12 13:57:40 +0100

    Fix FunConfigurationProducer

--- src/org/intellij/plugins/ceylon/runner/CeylonRunConfigurationProducer.java
+++ src/org/intellij/plugins/ceylon/runner/CeylonRunConfigurationProducer.java
@@ -4,20 +4,18 @@ import com.intellij.execution.actions.ConfigurationContext;
 import com.intellij.execution.actions.RunConfigurationProducer;
 import com.intellij.execution.configurations.ConfigurationType;
 import com.intellij.execution.configurations.RunConfiguration;
-import com.intellij.lang.ASTNode;
 import com.intellij.openapi.extensions.Extensions;
 import com.intellij.openapi.util.Ref;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiFile;
 import com.intellij.psi.util.PsiTreeUtil;
-import com.intellij.psi.util.PsiUtil;
 import com.intellij.util.containers.ContainerUtil;
-import com.redhat.ceylon.compiler.typechecker.tree.Node;
 import com.redhat.ceylon.compiler.typechecker.tree.Tree;
 import org.intellij.plugins.ceylon.psi.CeylonCompositeElement;
 import org.intellij.plugins.ceylon.psi.CeylonFile;
 import org.intellij.plugins.ceylon.psi.CeylonPsi;
-import org.intellij.plugins.ceylon.psi.CeylonTypes;
+
+import java.util.List;
 
 /**
  * TODO
@@ -35,12 +33,12 @@ public class CeylonRunConfigurationProducer extends RunConfigurationProducer<Run
         if (file instanceof CeylonFile) {
             CeylonRunConfiguration runConfig = (CeylonRunConfiguration) configuration;
 
-            final ASTNode identifier = getTopLevelMethodNode(srcElt);
+            final String identifier = getTopLevelMethodNode(srcElt);
 
             if (identifier != null) {
                 runConfig.setFilePath(file.getVirtualFile().getCanonicalPath());
-                runConfig.setTopLevelName(identifier.getText());
-                runConfig.setName(identifier.getText() + "() in " + file.getName());
+                runConfig.setTopLevelName(identifier);
+                runConfig.setName(identifier + "() in " + file.getName());
                 return true;
             }
         }
@@ -49,29 +47,35 @@ public class CeylonRunConfigurationProducer extends RunConfigurationProducer<Run
 
     @Override
     public boolean isConfigurationFromContext(RunConfiguration configuration, ConfigurationContext context) {
-        ASTNode identifier = null;
+        String identifier = null;
         if ((configuration instanceof CeylonRunConfiguration) && context.getLocation() != null) {
             identifier = getTopLevelMethodNode(context.getLocation().getPsiElement());
         }
 
-        return identifier != null && identifier.getText().equals(((CeylonRunConfiguration) configuration).getTopLevelName());
+        return identifier != null && identifier.equals(((CeylonRunConfiguration) configuration).getTopLevelName());
     }
 
-    private ASTNode getTopLevelMethodNode(PsiElement psiElement) {
+    private String getTopLevelMethodNode(PsiElement psiElement) {
 
         CeylonPsi.AnyMethodPsi method = PsiTreeUtil.getTopmostParentOfType(psiElement, CeylonPsi.AnyMethodPsi.class);
 
         if (method != null) {
-            final Node ceylonNode = method.getCeylonNode();
-            if (ceylonNode != null) {
-                CeylonCompositeElement parent = method;
-                // todo: make sure this doesn't loop infinitely
-                //noinspection StatementWithEmptyBody
-                while ((parent = PsiTreeUtil.getParentOfType(parent, CeylonPsi.DeclarationPsi.class)) != null && parent.getCeylonNode() == ceylonNode);
+            final Tree.AnyMethod ceylonMethod = (Tree.AnyMethod) method.getCeylonNode();
+            if (ceylonMethod != null) {
+                final List<Tree.ParameterList> parameterLists = ceylonMethod.getParameterLists();
+                if (!parameterLists.isEmpty()) {
+                    if (parameterLists.get(0).getParameters().isEmpty()) {
+                        CeylonCompositeElement parent = method;
+                        // todo: make sure this doesn't loop infinitely
+                        //noinspection StatementWithEmptyBody
+                        while ((parent = PsiTreeUtil.getParentOfType(parent, CeylonPsi.DeclarationPsi.class)) != null && parent.getCeylonNode() == ceylonMethod) ;
 
-                if (parent == null) {
-                    // We're in a method with no parent declaration (except possible nodes that represent the same method).
-                    return method.getNode().findChildByType(CeylonTypes.IDENTIFIER);
+                        if (parent == null) {
+                            // We're in a method with no parent declaration (except possible nodes that represent the same method).
+                            final Tree.Identifier methodIdent = ceylonMethod.getIdentifier();
+                            return methodIdent == null ? null : methodIdent.getText();
+                        }
+                    }
                 }
             }
         }
