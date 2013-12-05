ff4c3d1d06b105fc53ac2a4b71b118ce595f9926
Bastien Jansen <bjansen@excilys.com>
2013-12-05 22:29:12 +0100

    Avoid assigning default module to newly created files (see #32 and #24 and #22)

--- src/org/intellij/plugins/ceylon/action/CeylonAddModuleAction.java
+++ src/org/intellij/plugins/ceylon/action/CeylonAddModuleAction.java
@@ -7,31 +7,39 @@ import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.PlatformDataKeys;
 import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.components.ServiceManager;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.roots.ProjectRootManager;
 import com.intellij.openapi.ui.InputValidatorEx;
 import com.intellij.openapi.ui.Messages;
-import com.intellij.openapi.util.IconLoader;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.openapi.vfs.newvfs.impl.VirtualDirectoryImpl;
 import com.intellij.psi.PsiDirectory;
 import com.intellij.psi.PsiManager;
+import com.intellij.util.PlatformIcons;
+import com.redhat.ceylon.compiler.typechecker.TypeChecker;
+import com.redhat.ceylon.compiler.typechecker.io.impl.FileSystemVirtualFile;
+import com.redhat.ceylon.compiler.typechecker.model.Module;
+import org.intellij.plugins.ceylon.annotator.TypeCheckerManager;
 import org.jetbrains.annotations.Nullable;
 
+import java.io.File;
 import java.util.Properties;
 import java.util.regex.Pattern;
 
-// todo: check if module already exists (use ModuleIndex)
 public class CeylonAddModuleAction extends AnAction {
 
     public CeylonAddModuleAction() {
-        super(IconLoader.getIcon("/icons/ceylon.png"));
+        super(PlatformIcons.PACKAGE_ICON);
     }
 
     public void actionPerformed(final AnActionEvent e) {
-        VirtualFile eventDir = findEventDir(e);
+        final VirtualFile eventDir = findEventDir(e);
 
         if (eventDir != null && e.getProject() != null) {
+            TypeCheckerManager manager = ServiceManager.getService(e.getProject(), TypeCheckerManager.class);
+            final TypeChecker typeChecker = manager.getTypeChecker();
+
             VirtualFile srcRoot = getSourceRoot(e, eventDir);
             if (srcRoot != null) {
                 String eventPath = eventDir.getPath();
@@ -45,34 +53,7 @@ public class CeylonAddModuleAction extends AnAction {
                 final PsiDirectory eventPsiDir = PsiManager.getInstance(e.getProject()).findDirectory(eventDir);
 
                 // todo: choose module version in the same dialog
-                final String moduleName = Messages.showInputDialog(e.getProject(), "Enter module name", "Add Ceylon Module", null, null, new InputValidatorEx() {
-                    final Pattern pattern = Pattern.compile("([a-z_A-Z]|\\\\[iI])[a-z_A-Z0-9]*(\\.([a-z_A-Z]|\\\\[iI])[a-z_A-Z0-9]*)*");
-
-                    @Override
-                    public boolean checkInput(String name) {
-                        return pattern.matcher(name).matches() && !"default".equals(name)
-//                                && ModuleIndex.getInstance().get(name, project, GlobalSearchScope.projectScope(project)).isEmpty()
-                                ;
-                    }
-
-                    @Override
-                    public boolean canClose(String inputString) {
-                        return checkInput(inputString);
-                    }
-
-                    @Nullable
-                    @Override
-                    public String getErrorText(String name) {
-                        if (!pattern.matcher(name).matches()) {
-                            return String.format("\"%s\" is not a valid name for a module.", name);
-                        } else if ("default".equals(name)) {
-                            return "\"default\" is a reserved module name.";
-//                        } else if (!ModuleIndex.getInstance().get(name, project, GlobalSearchScope.projectScope(project)).isEmpty()) {
-//                            return String.format("Module %s already exists.", name);
-                        }
-                        return null;
-                    }
-                });
+                final String moduleName = Messages.showInputDialog(e.getProject(), "Enter module name", "Add Ceylon Module", null, null, new AddModuleInputValidator(typeChecker));
 
                 if (moduleName != null) {
                     ApplicationManager.getApplication().runWriteAction(new Runnable() {
@@ -92,6 +73,9 @@ public class CeylonAddModuleAction extends AnAction {
                             } catch (Exception e1) {
                                 Logger.getInstance(CeylonAddModuleAction.class).error("Can't create file from template", e1);
                             }
+
+                            // TODO if parent folder is "source", we basically parse eveything again... is this bad?
+                            typeChecker.getPhasedUnits().parseUnit(new FileSystemVirtualFile(new File(subdirectory.getVirtualFile().getParent().getCanonicalPath())));
                         }
                     });
                 }
@@ -110,12 +94,53 @@ public class CeylonAddModuleAction extends AnAction {
             eventSourceFile = eventSourceFile.getParent();
         }
 
-        return  eventSourceFile;
+        return eventSourceFile;
     }
 
     @Override
     public void update(AnActionEvent e) {
         final VirtualFile eventDir = findEventDir(e);
         e.getPresentation().setVisible(eventDir != null && getSourceRoot(e, eventDir) != null);
+    }
+
+    private static class AddModuleInputValidator implements InputValidatorEx {
+        final Pattern pattern = Pattern.compile("([a-z_A-Z]|\\\\[iI])[a-z_A-Z0-9]*(\\.([a-z_A-Z]|\\\\[iI])[a-z_A-Z0-9]*)*");
+        private TypeChecker typeChecker;
+
+        public AddModuleInputValidator(TypeChecker typeChecker) {
+
+            this.typeChecker = typeChecker;
+        }
+
+        @Override
+        public boolean checkInput(String name) {
+            return pattern.matcher(name).matches() && !moduleExists(name);
+        }
+
+        @Override
+        public boolean canClose(String inputString) {
+            return checkInput(inputString);
+        }
+
+        @Nullable
+        @Override
+        public String getErrorText(String name) {
+            if (!pattern.matcher(name).matches()) {
+                return String.format("\"%s\" is not a valid name for a module.", name);
+            } else if (moduleExists(name)) {
+                return String.format("Module %s already exists.", name);
+            }
+            return null;
+        }
+
+        private boolean moduleExists(String moduleName) {
+            for (Module module : typeChecker.getContext().getModules().getListOfModules()) {
+                if (module.getNameAsString().equals(moduleName)) {
+                    return true;
+                }
+            }
+
+            return false;
+        }
     }
 }
--- src/org/intellij/plugins/ceylon/annotator/CeylonTypeCheckerAnnotator.java
+++ src/org/intellij/plugins/ceylon/annotator/CeylonTypeCheckerAnnotator.java
@@ -10,6 +10,10 @@ import com.redhat.ceylon.compiler.typechecker.TypeChecker;
 import com.redhat.ceylon.compiler.typechecker.context.PhasedUnit;
 import com.redhat.ceylon.compiler.typechecker.io.VirtualFile;
 import com.redhat.ceylon.compiler.typechecker.io.impl.FileSystemVirtualFile;
+import com.redhat.ceylon.compiler.typechecker.model.Module;
+import com.redhat.ceylon.compiler.typechecker.model.Modules;
+import com.redhat.ceylon.compiler.typechecker.model.Package;
+import com.redhat.ceylon.compiler.typechecker.model.Unit;
 import com.redhat.ceylon.compiler.typechecker.parser.CeylonLexer;
 import com.redhat.ceylon.compiler.typechecker.parser.CeylonParser;
 import com.redhat.ceylon.compiler.typechecker.tree.Tree;
@@ -70,8 +74,8 @@ public class CeylonTypeCheckerAnnotator implements Annotator {
             }
         }
         if (phasedUnit == null) {
-            srcDir = new FileSystemVirtualFile(new File(ceylonFile.getVirtualFile().getParent().getPath()));
-            pkg = typeChecker.getContext().getModules().getDefaultModule().getPackages().get(0);
+            srcDir = getSourceFolder(ceylonFile);
+            pkg = getPackage(sourceCodeVirtualFile, srcDir, phasedUnit, typeChecker);
         } else {
             srcDir = phasedUnit.getSrcDir();
             pkg = phasedUnit.getPackage();
@@ -97,5 +101,78 @@ public class CeylonTypeCheckerAnnotator implements Annotator {
         if (phasedUnit.getCompilationUnit() != null) {
             phasedUnit.getCompilationUnit().visit(new CeylonTypeCheckerVisitor(holder));
         }
+    }
+
+    private VirtualFile getSourceFolder(CeylonFile sourceFile) {
+        com.intellij.openapi.vfs.VirtualFile sourceRoot = sourceFile.getVirtualFile().getParent();
+
+        if (sourceRoot == null) {
+            return null;
+        }
+
+        return new FileSystemVirtualFile(new File(sourceRoot.getPath()));
+    }
+
+    private Package getPackage(VirtualFile file, VirtualFile srcDir,
+                               PhasedUnit builtPhasedUnit, TypeChecker typeChecker) {
+        Package pkg = null;
+        if (builtPhasedUnit != null) {
+            // Editing an already built file
+            Package sourcePackage = builtPhasedUnit.getPackage();
+//            if (sourcePackage instanceof LazyPackage) {
+//                JDTModelLoader modelLoader = getProjectModelLoader(getProject());
+//                if (modelLoader != null) {
+//                    pkg = new LazyPackage(modelLoader);
+//                }
+//                else {
+//                    pkg = new Package();
+//                }
+//            }
+//            else {
+            pkg = new Package();
+//            }
+
+            pkg.setName(sourcePackage.getName());
+            pkg.setModule(sourcePackage.getModule());
+            for (Unit pkgUnit : sourcePackage.getUnits()) {
+                pkg.addUnit(pkgUnit);
+            }
+        } else {
+            // Editing a new file
+            Modules modules = typeChecker.getContext().getModules();
+            // Retrieve the target package from the file src-relative path
+            //TODO: this is very fragile!
+            String packageName = constructPackageName(file, srcDir);
+            for (Module module : modules.getListOfModules()) {
+                for (Package p : module.getPackages()) {
+                    if (p.getQualifiedNameString().equals(packageName)) {
+                        pkg = p;
+                        break;
+                    }
+                }
+                if (pkg != null) {
+                    break;
+                }
+            }
+            if (pkg == null) {
+                // assume the default package
+                pkg = modules.getDefaultModule().getPackages().get(0);
+
+                // TODO : iterate through parents to get the sub-package
+                // in which the package has been created, until we find the module
+                // Then the package can be created.
+                // However this should preferably be done on notification of the
+                // resource creation
+                // A more global/systematic integration between the model element
+                // (modules, packages, Units) and the IResourceModel should
+                // maybe be considered. But for now it is not required.
+            }
+        }
+        return pkg;
+    }
+
+    private String constructPackageName(VirtualFile file, VirtualFile srcDir) {
+        return file.getPath().substring(srcDir.getPath().length() + 1)
+                .replace("/" + file.getName(), "").replace('/', '.');
     }
 }
