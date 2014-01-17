db62bda1fb7cd00ccaa75beaf90ee063a4161571
matija.mazi@gmail.com <matija.mazi@gmail.com>
2014-01-17 11:48:13 +0100

    Expand test a bit #34

--- test/org/intellij/plugins/ceylon/annotator/CeylonSyntaxAnnotatorTest.java
+++ test/org/intellij/plugins/ceylon/annotator/CeylonSyntaxAnnotatorTest.java
@@ -8,13 +8,19 @@ public class CeylonSyntaxAnnotatorTest extends LightCodeInsightFixtureTestCase {
 
     @Override
     protected String getTestDataPath() {
-        return "testdata";
+        return "testdata/source";
     }
 
     public void testSyntaxAnnotator() throws Exception {
-        final PsiFile[] files = myFixture.configureByFiles("source/org/intellij/plugins/ceylon/annotator/SyntaxError.ceylon");
-        final PsiFile file = files[0];
-        TypeCheckerInvoker.invokeTypeChecker((CeylonFile) file);
-        myFixture.checkHighlighting(false, false, false);
+        testSyntax("SyntaxError", false, false, false);
+    }
+
+    private void testSyntax(String cuName, boolean checkWarnings, boolean checkInfos, boolean checkWeakWarnings) {
+        final PsiFile[] files = myFixture.configureByFiles("org/intellij/plugins/ceylon/annotator/" + cuName + ".ceylon");
+        final CeylonFile file = (CeylonFile) files[0];
+        TypeCheckerInvoker.invokeTypeChecker(file);
+//        System.out.println(file.getContainingDirectory());
+//        System.out.println(file.getCompilationUnit().getUnit());
+        myFixture.checkHighlighting(checkWarnings, checkInfos, checkWeakWarnings);
     }
 }
--- testdata/source/org/intellij/plugins/ceylon/annotator/SyntaxError.ceylon
+++ testdata/source/org/intellij/plugins/ceylon/annotator/SyntaxError.ceylon
@@ -1,3 +1,6 @@
+import ceylon.language {Integer}
+import <error descr="package not found in imported modules: ceylon.languagexyz">ceylon.languagexyz</error> {Integer}
+
 shared class Hello() {
     void sayHello() {
         <error descr="function or value does not exist: a">a</error> = 3;
