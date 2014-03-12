44f1f1d6aba60aed40c98f081a83623f0914ed04
mazi <matija.mazi@gmail.com>
2014-03-12 11:06:46 +0100

    #43 - Improve formatting according to @lucaswerkmeister's ceylon.formatter test samples. Added a test that uses ceylon.formatter's test-samples (requires ceylon.formatter in a sibling directory). Most tests pass, for the rest the formatted output at least seems sensible. No support for options.

--- src/org/intellij/plugins/ceylon/formatting/CeylonBlock.java
+++ src/org/intellij/plugins/ceylon/formatting/CeylonBlock.java
@@ -3,6 +3,7 @@ package org.intellij.plugins.ceylon.formatting;
 import com.intellij.formatting.*;
 import com.intellij.lang.ASTNode;
 import com.intellij.openapi.util.TextRange;
+import com.intellij.psi.TokenType;
 import com.intellij.psi.formatter.FormatterUtil;
 import com.intellij.psi.tree.IElementType;
 import org.intellij.plugins.ceylon.psi.CeylonTokens;
@@ -21,13 +22,25 @@ public class CeylonBlock implements Block {
 
     protected static final Spacing NO_SPACING = Spacing.createSpacing(0, 0, 0, false, 0);
     protected static final Spacing EMPTY_LINE_SPACING = Spacing.createSpacing(0, 0, 2, true, 0);
-    protected static final Spacing GOTO_LINE_SPACING = Spacing.createSpacing(0, 0, 1, true, 1);
-    protected static final Spacing SINGLE_SPACE_SPACING = Spacing.createSpacing(1, 1, 0, true, 1);
+    protected static final Spacing NEW_LINE_SPACING = Spacing.createSpacing(0, 0, 1, true, 1);
+    protected static final Spacing NEW_LINE_SPACING_STRICT = Spacing.createSpacing(0, 0, 1, false, 0);
+    protected static final Spacing SINGLE_SPACE_SPACING = Spacing.createSpacing(1, 1, 0, true, 0);
+    protected static final Spacing SINGLE_SPACE_SPACING_INLINE = Spacing.createSpacing(1, 1, 0, false, 0);
 
-    protected static final List<IElementType> BLOCK_TYPES =
-            Arrays.asList(CeylonTypes.IMPORT_MODULE_LIST, CeylonTypes.BLOCK, CeylonTypes.CLASS_BODY, CeylonTypes.INTERFACE_BODY, CeylonTypes.NAMED_ARGUMENT_LIST, CeylonTypes.SEQUENCE_ENUMERATION);
+    protected static final List<IElementType> SMALLER_OP = Arrays.asList(CeylonTypes.SMALLER_OP, CeylonTokens.SMALLER_OP);
+    protected static final List<IElementType> LARGER_OP = Arrays.asList(CeylonTypes.LARGER_OP, CeylonTokens.LARGER_OP);
+    protected static final List<IElementType> INDENT_CHILDREN_NORMAL = Arrays.asList(
+            CeylonTypes.IMPORT_MODULE_LIST, CeylonTypes.BLOCK, CeylonTypes.CLASS_BODY, CeylonTypes.INTERFACE_BODY,
+            CeylonTypes.NAMED_ARGUMENT_LIST, CeylonTypes.SEQUENCED_ARGUMENT, CeylonTypes.IMPORT_MEMBER_OR_TYPE_LIST
+    );
+    protected static final List<IElementType> INDENT_CHILDREN_NONE = Arrays.asList(
+            CeylonTypes.CEYLON_FILE, CeylonTypes.COMPILATION_UNIT,
+            CeylonTypes.SWITCH_STATEMENT, CeylonTypes.SWITCH_CASE_LIST, CeylonTypes.SWITCH_CLAUSE, CeylonTypes.CASE_CLAUSE,
+            CeylonTypes.SEQUENCE_ENUMERATION, CeylonTypes.ANNOTATION_LIST, CeylonTypes.TYPE_CONSTRAINT_LIST
+    );
 
     public CeylonBlock(ASTNode node, Indent indent) {
+        System.out.printf("Indent for %s: %s%n", node.getElementType(), indent);
         this.node = node;
         this.indent = indent;
     }
@@ -43,14 +56,25 @@ public class CeylonBlock implements Block {
     public List<Block> getSubBlocks() {
         List<Block> blocks = new ArrayList<>();
 
+        final IElementType nodeType = node.getElementType();
+        final Indent normalChildIndent = INDENT_CHILDREN_NORMAL.contains(nodeType) ? Indent.getNormalIndent()
+                        : INDENT_CHILDREN_NONE.contains(nodeType) ? Indent.getNoneIndent()
+                        : null;
+//        System.out.printf("normalChildIndent for %s: %s%n", nodeType, indent);
+        IElementType prevChildType = null;
         for (ASTNode child = node.getFirstChildNode(); child != null; child = child.getTreeNext()) {
             if (!FormatterUtil.containsWhiteSpacesOnly(child) && child.getTextLength() > 0) {
-                if (BLOCK_TYPES.contains(node.getElementType()) && child.getElementType() != CeylonTokens.RBRACE) {
-                    blocks.add(new CeylonBlock(child, Indent.getNormalIndent()));
-                } else {
-//                    System.out.println("No ident for " + child.getElementType());
-                    blocks.add(new CeylonBlock(child, Indent.getNoneIndent()));
+                Indent indent = normalChildIndent;
+                if (child.getElementType() == CeylonTokens.RBRACE || child.getElementType() == CeylonTokens.LBRACE) {
+                    indent = Indent.getNoneIndent();
                 }
+                if (prevChildType == CeylonTypes.ANNOTATION_LIST && indent == null) {
+                    indent = Indent.getNoneIndent();
+                }
+                blocks.add(new CeylonBlock(child, indent));
+            }
+            if (child.getElementType() != TokenType.WHITE_SPACE) {
+                prevChildType = child.getElementType();
             }
         }
 
@@ -81,6 +105,12 @@ public class CeylonBlock implements Block {
         CeylonBlock block1 = (CeylonBlock) child1;
         CeylonBlock block2 = (CeylonBlock) child2;
 
+        IElementType type1 = block1 == null ? null : block1.node.getElementType();
+        final IElementType type2 = block2.node.getElementType();
+
+        Spacing result = null;
+
+        final IElementType nodeType = node.getElementType();
         Collection<IElementType> typesRequiringNoLeftSpacing = Arrays.asList(
                 CeylonTypes.PARAMETER_LIST, CeylonTokens.RPAREN, CeylonTokens.COMMA, CeylonTokens.SEMICOLON,
                 CeylonTypes.MEMBER_OP, CeylonTokens.MEMBER_OP,
@@ -88,10 +118,11 @@ public class CeylonBlock implements Block {
                 CeylonTokens.UNION_OP, CeylonTypes.UNION_OP, CeylonTokens.INTERSECTION_OP, CeylonTypes.INTERSECTION_OP,
                 CeylonTypes.TYPE_ARGUMENT_LIST,
                 CeylonTypes.SEQUENCED_TYPE, CeylonTokens.OPTIONAL, CeylonTypes.POSITIONAL_ARGUMENT_LIST, CeylonTokens.RBRACKET,
-                CeylonTypes.INCREMENT_OP, CeylonTypes.DECREMENT_OP, CeylonTypes.POSTFIX_INCREMENT_OP, CeylonTypes.POSTFIX_DECREMENT_OP, 
+                CeylonTypes.INCREMENT_OP, CeylonTypes.DECREMENT_OP, CeylonTypes.POSTFIX_INCREMENT_OP, CeylonTypes.POSTFIX_DECREMENT_OP,
                 CeylonTypes.SAFE_MEMBER_OP,
                 CeylonTypes.RANGE_OP, CeylonTokens.RANGE_OP,
-                CeylonTokens.LBRACKET
+                CeylonTokens.LBRACKET,
+                CeylonTokens.ENTRY_OP, CeylonTypes.ENTRY_OP
         );
         Collection<IElementType> typesRequiringNoRightSpacing = Arrays.asList(
                 CeylonTokens.LPAREN,
@@ -101,45 +132,60 @@ public class CeylonBlock implements Block {
                 CeylonTypes.INCREMENT_OP, CeylonTypes.DECREMENT_OP,
                 CeylonTypes.SEQUENCED_TYPE, CeylonTokens.LBRACKET, CeylonTypes.SAFE_MEMBER_OP,
                 CeylonTypes.RANGE_OP, CeylonTokens.RANGE_OP,
-                CeylonTypes.NOT_OP,
-                CeylonTypes.SPREAD_OP
-        );
-        Collection<IElementType> typesRequiringNewLine = Arrays.asList(
-                CeylonTypes.DECLARATION, CeylonTypes.IMPORT_MODULE
+                CeylonTypes.SPREAD_OP,
+                CeylonTokens.ENTRY_OP, CeylonTypes.ENTRY_OP,
+                CeylonTypes.POSITIVE_OP, CeylonTypes.NEGATIVE_OP
         );
         Collection<IElementType> typesRequiringEmptyLine = Arrays.asList(
-                CeylonTypes.IMPORT_LIST, CeylonTypes.DECLARATION
+                CeylonTypes.IMPORT_LIST, CeylonTypes.DECLARATION, CeylonTypes.METHOD_DEFINITION
         );
 
-        IElementType type1 = block1 == null ? null : block1.node.getElementType();
-        final IElementType type2 = block2.node.getElementType();
-
-        Spacing result = null;
-        if (block1 != null) {
-            if (typesRequiringEmptyLine.contains(type1)) {
+        if (block1 == null) {
+            result = NO_SPACING;
+        } else {
+            if (typesRequiringEmptyLine.contains(type1) && type2 != CeylonTokens.RBRACE
+                    || typesRequiringEmptyLine.contains(type2) && !Arrays.asList(CeylonTokens.LBRACE, CeylonTypes.ANNOTATION_LIST).contains(type1)) {
                 result = EMPTY_LINE_SPACING;
-            }
-            if (typesRequiringNewLine.contains(type1) || (type1 == CeylonTokens.LBRACE &&
-                    (block1.node.getTreeParent().getElementType() == CeylonTypes.BLOCK || block1.node.getTreeParent().getElementType() == CeylonTypes.IMPORT_MODULE_LIST))) {
-                result = GOTO_LINE_SPACING;
-            }
-            if (typesRequiringNoRightSpacing.contains(type1) || typesRequiringNoLeftSpacing.contains(type2)
+            } else if (Arrays.asList(CeylonTypes.IMPORT_MODULE).contains(type1)) {
+                result = NEW_LINE_SPACING;
+            } else if (Arrays.asList(CeylonTypes.IMPORT_MEMBER).contains(type2)) {
+                result = NEW_LINE_SPACING;
+            } else if (typesRequiringNoRightSpacing.contains(type1) || typesRequiringNoLeftSpacing.contains(type2)
                     || (type1 == CeylonTypes.SMALLER_OP && type2 == CeylonTypes.TYPE_PARAMETER_DECLARATION)
                     || (type1 == CeylonTypes.TYPE_PARAMETER_LITERAL && type2 == CeylonTypes.LARGER_OP)) {
                 result = NO_SPACING;
+            } else if (CeylonTokens.LINE_COMMENT.equals(type1)) {
+                // This is wrong because it inserts a linebreak after line comments, but better than not doing anything.
+                // The problem is that the lexer treats the newline as part of the line comment, so whatever follows
+                // the line comment is considered to be in the same line.
+                result = NEW_LINE_SPACING;
+            } else if ((SMALLER_OP.contains(type1) && isType(block2))
+                    || (LARGER_OP.contains(type2) && isType(block1))) {
+                result = NO_SPACING;
             }
         }
-        
+
+        Collection<IElementType> bothTypes = Arrays.asList(type1, type2);
         if (result == null) {
-//            final List<IElementType> bothTypes = Arrays.asList(type1, type2);
-            if (Arrays.asList(CeylonTypes.TYPE_ARGUMENT_LIST, CeylonTypes.TYPE_PARAMETER_LIST, CeylonTypes.SEQUENCED_TYPE, CeylonTypes.INDEX_EXPRESSION).contains(node.getElementType())) {
+            if (Arrays.asList(CeylonTypes.TYPE_ARGUMENT_LIST, CeylonTypes.TYPE_PARAMETER_LIST, CeylonTypes.SEQUENCED_TYPE, CeylonTypes.INDEX_EXPRESSION).contains(nodeType)) {
                 result = NO_SPACING;
-            }
-            if (BLOCK_TYPES.contains(node.getElementType()) && type1 == CeylonTokens.LBRACE && type2 == CeylonTokens.RBRACE) {
+            } else if (type1 == CeylonTokens.LBRACE && type2 == CeylonTokens.RBRACE) {
                 result = NO_SPACING;
-            }
-            if (CeylonTypes.SPREAD_ARGUMENT.equals(node.getElementType()) && CeylonTokens.PRODUCT_OP.equals(type1)) {
-                return NO_SPACING;
+            } else if (CeylonTypes.SPREAD_ARGUMENT.equals(nodeType) && CeylonTokens.PRODUCT_OP.equals(type1)) {
+                result = NO_SPACING;
+            } else if (CeylonTypes.FUNCTION_TYPE.equals(nodeType)) {
+                result = CeylonTokens.COMMA.equals(type1) ? SINGLE_SPACE_SPACING : NO_SPACING;
+            } else if (Arrays.asList(CeylonTypes.IMPORT_MEMBER, CeylonTypes.THROW, CeylonTypes.IMPORT, CeylonTypes.ASSERTION, CeylonTypes.SATISFIED_TYPES, CeylonTypes.EXTENDED_TYPE).contains(nodeType)) {
+                result = SINGLE_SPACE_SPACING_INLINE;
+            } else if (nodeType == CeylonTypes.ANNOTATION_LIST && type1 == CeylonTypes.STRING_LITERAL) {
+                result = NEW_LINE_SPACING;
+            } else if (Arrays.asList(CeylonTypes.METHOD_DEFINITION).contains(nodeType) && type2 == CeylonTypes.BLOCK) {
+                result = SINGLE_SPACE_SPACING_INLINE;
+            } else if (bothTypes.contains(CeylonTypes.IDENTIFIER) && Arrays.asList(CeylonTypes.CLASS_DEFINITION, CeylonTypes.INTERFACE_DEFINITION).contains(nodeType)) {
+                result = SINGLE_SPACE_SPACING_INLINE;
+            } else if (INDENT_CHILDREN_NORMAL.contains(nodeType)
+                        && (type2 == CeylonTokens.RBRACE && type1 != CeylonTokens.LBRACE || type2 != CeylonTokens.RBRACE && type1 == CeylonTokens.LBRACE)) {
+                result = NEW_LINE_SPACING_STRICT;
             }
         }
 
@@ -147,13 +193,17 @@ public class CeylonBlock implements Block {
             result = SINGLE_SPACE_SPACING;
         }
 
-//        System.out.printf("Spacing between %s and %s in %s: %s%n", type1, type2, node.getElementType(), getSpacingName(result));
+        System.out.printf("Spacing between %s and %s in %s: %s%n", type1, type2, nodeType, getSpacingName(result));
         return result;
+    }
+
+    private boolean isType(CeylonBlock block) {
+        return block.node.getElementType().toString().endsWith("TYPE");
     }
 
     private Object getSpacingName(Spacing result) {
         return result == EMPTY_LINE_SPACING ? "EMPTY_LINE_SPACING"
-                : result == GOTO_LINE_SPACING ? "GOTO_LINE_SPACING"
+                : result == NEW_LINE_SPACING ? "NEW_LINE_SPACING"
                 : result == NO_SPACING ? "NO_SPACING"
                 : result == SINGLE_SPACE_SPACING ? "SINGLE_SPACE_SPACING"
                 : result;
---
+++ test/org/intellij/plugins/ceylon/formatting/CeylonFormatterTest.java
@@ -0,0 +1,135 @@
+package org.intellij.plugins.ceylon.formatting;
+
+import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.psi.PsiFile;
+import com.intellij.psi.codeStyle.CodeStyleManager;
+import com.intellij.testFramework.fixtures.LightCodeInsightFixtureTestCase;
+import org.intellij.plugins.ceylon.psi.CeylonFile;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.nio.file.Paths;
+
+/**
+ * NOTE: this requires that the ceylon.formatter directory is a sibling directory to the ceylon-ide-intellij.
+ */
+public class CeylonFormatterTest extends LightCodeInsightFixtureTestCase {
+
+    @Override
+    protected String getTestDataPath() {
+        return "../ceylon.formatter/test-samples";
+    }
+
+    // pass:
+    public void testFunctionTypes() throws Exception { testFormatting("functionTypes"); }
+    public void testSpreadArguments() throws Exception { testFormatting("spreadArguments"); }
+    public void testGroupedTypes() throws Exception { testFormatting("groupedTypes"); }
+    public void testNot() throws Exception { testFormatting("not"); }
+    public void testAttributeGetterDeclaration() throws Exception { testFormatting("attributeGetterDeclaration"); }
+    public void testSwitch() throws Exception { testFormatting("switch"); }
+    public void testBinaryOperators() throws Exception { testFormatting("binaryOperators"); }
+    public void testOuter() throws Exception { testFormatting("outer"); }
+    public void testIf() throws Exception { testFormatting("if"); }
+    public void testTry() throws Exception { testFormatting("try"); }
+    public void testBreak() throws Exception { testFormatting("break"); }
+    public void testTuples() throws Exception { testFormatting("tuples"); }
+    public void testCaseTypes() throws Exception { testFormatting("caseTypes"); }
+    public void testPositiveNegativeOp() throws Exception { testFormatting("positiveNegativeOp"); }
+    public void testTypeAliases() throws Exception { testFormatting("typeAliases"); }
+    public void testIndexExpressions() throws Exception { testFormatting("indexExpressions"); }
+    public void testPostfixOperators() throws Exception { testFormatting("postfixOperators"); }
+    public void testTypeArguments() throws Exception { testFormatting("typeArguments"); }
+    public void testComprehensions() throws Exception { testFormatting("comprehensions"); }
+    public void testInterfaces() throws Exception { testFormatting("interfaces"); }
+    public void testPrefixOperators() throws Exception { testFormatting("prefixOperators"); }
+    public void testContinue() throws Exception { testFormatting("continue"); }
+    public void testRangeOp() throws Exception { testFormatting("rangeOp"); }
+    public void testTypes() throws Exception { testFormatting("types"); }
+    public void testReturn() throws Exception { testFormatting("return"); }
+    public void testWhile() throws Exception { testFormatting("while"); }
+    public void testExistsNonempty() throws Exception { testFormatting("existsNonempty"); }
+    public void testSelf() throws Exception { testFormatting("self"); }
+    public void testWithinOps() throws Exception { testFormatting("withinOps"); }
+    public void testExpressions() throws Exception { testFormatting("expressions"); }
+    public void testSequencedArguments() throws Exception { testFormatting("sequencedArguments"); }
+    public void testFor() throws Exception { testFormatting("for"); }
+    public void testModule() throws Exception { testFormatting("module"); }
+    public void testPackage() throws Exception { testFormatting("package"); }
+    public void testQualifiedMemberOrTypeExpression() throws Exception { testFormatting("qualifiedMemberOrTypeExpressions"); }
+    public void testTypeConstraints() throws Exception { testFormatting("typeConstraints"); }
+    public void testAnnotationsNoArguments() throws Exception { testFormatting("annotationsNoArguments"); }
+
+
+
+    // only end-of-file WS handling etc.:
+    public void testAssignments() throws Exception { testFormatting("assignments"); }
+    public void testStringTemplates() throws Exception { testFormatting("stringTemplates"); }
+    public void testHelloWorld() throws Exception { testFormatting("helloWorld"); }
+    public void testHelloWorldCommented() throws Exception { testFormatting("helloWorldCommented"); }
+    public void testObjectArguments() throws Exception { testFormatting("objectArguments"); }
+
+    // only line comment newlines:
+    public void testThrow() throws Exception { testFormatting("throw"); }
+    public void testCommentsAfterStatements() throws Exception { testFormatting("commentsAfterStatements"); }
+    public void testSimpleClass() throws Exception { testFormatting("simpleClass"); }
+
+
+    // only options:
+    public void testBraceOnOwnLine() throws Exception { testFormatting("braceOnOwnLine"); }
+    public void testParamListParenWithSpaces() throws Exception { testFormatting("paramListParenWithSpaces"); }
+    public void testParamListParenWithoutSpaces() throws Exception { testFormatting("paramListParenWithoutSpaces"); }
+    public void testImportSingleLine() throws Exception { testFormatting("importSingleLine"); }
+    public void testTypeOperatorExpressions() throws Exception { testFormatting("typeOperatorExpressions"); }
+
+    // I don't agree (eg. =)
+    public void testImportKeywords() throws Exception { testFormatting("importKeywords"); }
+
+
+    ////////////////////////////////////////////////
+    // fail:
+
+    // newlines before RBRACE
+    public void testSmallBlocks() throws Exception { testFormatting("smallBlocks"); }
+    public void testIs() throws Exception { testFormatting("is"); }
+    public void testObjects() throws Exception { testFormatting("objects"); }
+
+
+    // other fail:
+    public void testFunctionArguments() throws Exception { testFormatting("functionArguments"); }
+    public void testMultiLineStringIndented() throws Exception { testFormatting("multiLineStringIndented"); }
+    public void testAnnotationsPositionalArguments() throws Exception { testFormatting("annotationsPositionalArguments"); }
+    public void testNamedArguments() throws Exception { testFormatting("namedArguments"); }
+    public void testImportMultiLine() throws Exception { testFormatting("importMultiLine"); }
+    public void testDoc() throws Exception { testFormatting("doc"); }
+    public void testLongInvocation() throws Exception { testFormatting("longInvocation"); }
+    public void testMemberOp() throws Exception { testFormatting("memberOp"); }
+    public void testMultiLineParameterList() throws Exception { testFormatting("multiLineParameterList"); }
+    public void testMultiLineString() throws Exception { testFormatting("multiLineString"); }
+    public void testComments() throws Exception { testFormatting("comments"); }
+
+
+    private void testFormatting(String testSource) throws Exception {
+        testSource = testSource + ".ceylon";
+        final PsiFile[] files = myFixture.configureByFiles(testSource);
+        final CeylonFile file = (CeylonFile) files[0];
+        final File options = Paths.get(getTestDataPath(), testSource + ".options").toFile();
+        if (options.exists()) {
+            System.out.println("---- Options found: " + file.getName());
+            final BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(options)));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+            System.out.println("--------");
+        }
+
+        ApplicationManager.getApplication().runWriteAction(new Runnable() {
+            @Override public void run() {
+                CodeStyleManager.getInstance(getProject()).reformat(file);
+            }
+        });
+        myFixture.checkResultByFile(testSource + ".formatted");
+    }
+}
