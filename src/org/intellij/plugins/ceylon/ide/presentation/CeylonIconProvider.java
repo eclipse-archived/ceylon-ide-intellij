adb05b6e1e59a3743d6bd226604cf8fe5b5a471f
Bastien Jansen <bjansen@excilys.com>
2015-01-27 22:22:51 +0100

    Testing custom icon based on file name/content

---
+++ src/org/intellij/plugins/ceylon/ide/presentation/CeylonIconProvider.java
@@ -0,0 +1,29 @@
+package org.intellij.plugins.ceylon.ide.presentation;
+
+import com.intellij.ide.IconProvider;
+import com.intellij.psi.PsiElement;
+import com.intellij.util.PlatformIcons;
+import com.redhat.ceylon.compiler.typechecker.analyzer.ModuleManager;
+import org.intellij.plugins.ceylon.ide.psi.CeylonFile;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import javax.swing.*;
+
+public class CeylonIconProvider extends IconProvider {
+    @Nullable
+    @Override
+    public Icon getIcon(@NotNull PsiElement element, int flags) {
+        if (element instanceof CeylonFile) {
+            String fileName = ((CeylonFile) element).getName();
+
+            if (fileName.equals(ModuleManager.PACKAGE_FILE)) {
+                return PlatformIcons.PACKAGE_ICON;
+            } else if (fileName.equals(ModuleManager.MODULE_FILE)) {
+                return PlatformIcons.CLOSED_MODULE_GROUP_ICON;
+            }
+        }
+
+        return null;
+    }
+}
