254a0deb71d6d5fce725684b72b1b55c9a719eee
Bastien Jansen <bastien.jansen@gmx.com>
2015-11-12 18:52:37 +0100

    Fixed #91: things not working in IntelliJ 14.

--- src/org/intellij/plugins/ceylon/ide/runner/CeylonJsRunConfigurationType.java
+++ src/org/intellij/plugins/ceylon/ide/runner/CeylonJsRunConfigurationType.java
@@ -5,14 +5,15 @@ import com.intellij.execution.configurations.ConfigurationTypeBase;
 import com.intellij.execution.configurations.RunConfiguration;
 import com.intellij.execution.configurations.RunConfigurationModule;
 import com.intellij.openapi.project.Project;
-import org.intellij.plugins.ceylon.ide.ceylonCode.util.ideaIcons_;
+import com.intellij.openapi.util.IconLoader;
 import org.jetbrains.annotations.NotNull;
 
 public class CeylonJsRunConfigurationType extends ConfigurationTypeBase {
 
     protected CeylonJsRunConfigurationType() {
+        // using ideaIcons fails in IntelliJ 14...
         super("CeylonJsRunConfiguration", "Ceylon JavaScript",
-                "Ceylon JS runner", ideaIcons_.get_().getCeylon());
+                "Ceylon JS runner", IconLoader.getIcon("/icons/ceylon.png"));
 
         addFactory(new ConfigurationFactory(this) {
             @NotNull
