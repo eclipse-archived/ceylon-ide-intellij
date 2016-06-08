7965223c1718754d52ec475aa101254589323fd9
Bastien Jansen <bastien.jansen@gmx.com>
2016-06-08 18:41:54 +0200

    Fixed #230 (NPE in structure view while loading a project).

--- src/org/intellij/plugins/ceylon/ide/structureView/CeylonInheritedMembersNodeProvider.java
+++ src/org/intellij/plugins/ceylon/ide/structureView/CeylonInheritedMembersNodeProvider.java
@@ -11,6 +11,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.concurrent.Callable;
 
+import com.redhat.ceylon.model.typechecker.model.*;
 import org.intellij.plugins.ceylon.ide.ceylonCode.model.ConcurrencyManagerForJava;
 import org.intellij.plugins.ceylon.ide.ceylonCode.psi.CeylonCompositeElement;
 import org.intellij.plugins.ceylon.ide.ceylonCode.psi.CeylonFile;
@@ -36,11 +37,6 @@ import com.redhat.ceylon.ide.common.util.FindDeclarationNodeVisitor;
 import com.redhat.ceylon.model.loader.model.FieldValue;
 import com.redhat.ceylon.model.loader.model.JavaMethod;
 import com.redhat.ceylon.model.loader.model.LazyClass;
-import com.redhat.ceylon.model.typechecker.model.Declaration;
-import com.redhat.ceylon.model.typechecker.model.DeclarationWithProximity;
-import com.redhat.ceylon.model.typechecker.model.TypeDeclaration;
-import com.redhat.ceylon.model.typechecker.model.TypeParameter;
-import com.redhat.ceylon.model.typechecker.model.Unit;
 import org.intellij.plugins.ceylon.ide.ceylonCode.model.PSIClass;
 import org.intellij.plugins.ceylon.ide.ceylonCode.model.PSIMethod;
 
@@ -57,61 +53,65 @@ class CeylonInheritedMembersNodeProvider extends InheritedMembersNodeProvider {
             PsiElement element = ((PsiTreeElementBase) node).getElement();
             if (element instanceof CeylonPsi.DeclarationPsi) {
                 Tree.Declaration declaration = ((CeylonPsi.DeclarationPsi) element).getCeylonNode();
-                List<TreeElement> elements = new ArrayList<>();
 
                 TypeDeclaration type;
                 if (declaration instanceof Tree.ObjectDefinition) {
-                    type = ((Tree.ObjectDefinition) declaration).getDeclarationModel().getTypeDeclaration();
+                    Value model = ((Tree.ObjectDefinition) declaration).getDeclarationModel();
+                    type = model == null ? null : model.getTypeDeclaration();
                 } else if (declaration instanceof Tree.ClassOrInterface) {
                     type = ((Tree.ClassOrInterface) declaration).getDeclarationModel();
                 } else {
                     type = null;
                 }
 
-                if (type == null) {
-                    // Maybe the file hasn't been typechecked yet
-                    return elements;
+                if (type != null) {
+                    return scanMembers(element, declaration, type);
                 }
 
-                Map<String, DeclarationWithProximity> decls = type.getMatchingMemberDeclarations(declaration.getUnit(), type, "", 0, null);
+                // else: Maybe the file hasn't been typechecked yet
+            }
+        }
+        return Collections.emptyList();
+    }
 
-                for (DeclarationWithProximity dwp : decls.values()) {
-                    for (Declaration decl : toJavaIterable(klass(Declaration.class),  overloads(dwp.getDeclaration()))) {
-                        if (!(decl instanceof TypeParameter)) {
-                            Unit unit = decl.getUnit();
-                            PsiFile file = unit.equals(declaration.getUnit())
-                                    ? element.getContainingFile()
-                                    : CeylonTreeUtil.getDeclaringFile(unit, element.getProject());
+    private List<TreeElement> scanMembers(PsiElement element, Tree.Declaration declaration, TypeDeclaration type) {
+        List<TreeElement> elements = new ArrayList<>();
 
-                            if (file instanceof CeylonFile) {
-                                boolean inherited = type.isInherited(decl);
-                                StructureViewTreeElement treeElement = getTreeElementForDeclaration((CeylonFile) file, decl, inherited);
+        Map<String, DeclarationWithProximity> decls = type.getMatchingMemberDeclarations(declaration.getUnit(), type, "", 0, null);
 
-                                if (treeElement != null) {
-                                    elements.add(treeElement);
-                                }
-                            } else {
-                                // TODO perhaps we can provide our own TreeElements that are visually identical to the others?
-                                // perhaps a Java declaration?
-                                if (decl instanceof JavaMethod) {
-                                    PsiMethod method = ((PSIMethod) ((JavaMethod) decl).mirror).getPsi();
-                                    elements.add(new PsiMethodTreeElement(method, true));
-                                } else if (decl instanceof FieldValue && decl.getScope() instanceof LazyClass) {
-                                    PsiClass cls = ((PSIClass) ((LazyClass) decl.getScope()).classMirror).getPsi();
-                                    PsiField field = cls.findFieldByName(decl.getName(), true);
-                                    if (field != null) {
-                                        elements.add(new PsiFieldTreeElement(field, true));
-                                    }
-                                }
+        for (DeclarationWithProximity dwp : decls.values()) {
+            for (Declaration decl : toJavaIterable(klass(Declaration.class), overloads(dwp.getDeclaration()))) {
+                if (!(decl instanceof TypeParameter)) {
+                    Unit unit = decl.getUnit();
+                    PsiFile file = unit.equals(declaration.getUnit())
+                            ? element.getContainingFile()
+                            : CeylonTreeUtil.getDeclaringFile(unit, element.getProject());
+
+                    if (file instanceof CeylonFile) {
+                        boolean inherited = type.isInherited(decl);
+                        StructureViewTreeElement treeElement = getTreeElementForDeclaration((CeylonFile) file, decl, inherited);
+
+                        if (treeElement != null) {
+                            elements.add(treeElement);
+                        }
+                    } else {
+                        // perhaps a Java declaration?
+                        if (decl instanceof JavaMethod) {
+                            PsiMethod method = ((PSIMethod) ((JavaMethod) decl).mirror).getPsi();
+                            elements.add(new PsiMethodTreeElement(method, true));
+                        } else if (decl instanceof FieldValue && decl.getScope() instanceof LazyClass) {
+                            PsiClass cls = ((PSIClass) ((LazyClass) decl.getScope()).classMirror).getPsi();
+                            PsiField field = cls.findFieldByName(decl.getName(), true);
+                            if (field != null) {
+                                elements.add(new PsiFieldTreeElement(field, true));
                             }
                         }
                     }
                 }
-
-                return elements;
             }
         }
-        return Collections.emptyList();
+
+        return elements;
     }
 
     private StructureViewTreeElement getTreeElementForDeclaration(final CeylonFile myFile,
