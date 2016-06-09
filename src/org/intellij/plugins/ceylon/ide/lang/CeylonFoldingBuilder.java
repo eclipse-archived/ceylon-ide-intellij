757c1b081fb8be28daf97729ec2c989ade17bdd5
Bastien Jansen <bastien.jansen@gmx.com>
2016-06-09 10:19:50 +0200

    Fold multiline string literals, multiline comments and consecutive comments (fixes #241).

--- src/org/intellij/plugins/ceylon/ide/lang/CeylonFoldingBuilder.java
+++ src/org/intellij/plugins/ceylon/ide/lang/CeylonFoldingBuilder.java
@@ -6,12 +6,15 @@ import com.intellij.lang.folding.FoldingDescriptor;
 import com.intellij.openapi.editor.Document;
 import com.intellij.openapi.util.TextRange;
 import com.intellij.psi.PsiElement;
+import com.intellij.psi.util.PsiTreeUtil;
 import org.intellij.plugins.ceylon.ide.ceylonCode.psi.CeylonPsi;
+import org.intellij.plugins.ceylon.ide.ceylonCode.psi.CeylonTokens;
 import org.intellij.plugins.ceylon.ide.ceylonCode.psi.CeylonTypes;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 
@@ -30,6 +33,34 @@ public class CeylonFoldingBuilder extends FoldingBuilderEx {
     private void appendDescriptors(PsiElement root, List<FoldingDescriptor> descriptors) {
         if (root instanceof CeylonPsi.BodyPsi) {
             descriptors.add(new FoldingDescriptor(root.getNode(), root.getTextRange()));
+        } else if (root.getNode().getElementType() == CeylonTokens.MULTI_COMMENT) {
+            foldAfterFirstLine(root, descriptors);
+        } else if (root.getNode().getElementType() == CeylonTokens.LINE_COMMENT) {
+            PsiElement prev = PsiTreeUtil.prevVisibleLeaf(root);
+            if (prev == null || prev.getNode().getElementType() != CeylonTokens.LINE_COMMENT) {
+                PsiElement lastComment = root;
+                PsiElement next;
+
+                do {
+                    next = PsiTreeUtil.nextVisibleLeaf(lastComment);
+                    if (next != null
+                            && next.getNode().getElementType() == CeylonTokens.LINE_COMMENT) {
+                        lastComment = next;
+                    } else {
+                        next = null;
+                    }
+                } while (next != null);
+
+                if (lastComment != root) {
+                    TextRange range = new TextRange(
+                            root.getTextRange().getEndOffset() - 1,
+                            lastComment.getTextRange().getEndOffset() - 1
+                    );
+                    descriptors.add(new FoldingDescriptor(root.getNode(), range));
+                }
+            }
+        } else if (root instanceof CeylonPsi.StringLiteralPsi) {
+            foldAfterFirstLine(root, descriptors);
         } else if (root instanceof CeylonPsi.ImportListPsi && root.getTextLength() > 0) {
             int start = root.getTextRange().getStartOffset();
             if (root.getText().startsWith("import ")) {
@@ -42,15 +73,29 @@ public class CeylonFoldingBuilder extends FoldingBuilderEx {
             descriptors.add(new FoldingDescriptor(root.getNode(), range));
         }
 
-        for (PsiElement child : root.getChildren()) {
+        PsiElement child = root.getFirstChild();
+
+        while (child != null) {
             appendDescriptors(child, descriptors);
+            child = child.getNextSibling();
+        }
+    }
+
+    private void foldAfterFirstLine(PsiElement root, List<FoldingDescriptor> descriptors) {
+        int newLine = root.getText().indexOf('\n');
+        if (newLine > 0) {
+            TextRange range = new TextRange(root.getTextRange().getStartOffset() + newLine,
+                    root.getTextRange().getEndOffset());
+            descriptors.add(new FoldingDescriptor(root.getNode(), range));
         }
     }
 
     @Nullable
     @Override
     public String getPlaceholderText(@NotNull ASTNode node) {
-        return node.getElementType() == CeylonTypes.IMPORT_LIST ? "..." : "{...}";
+        return Arrays.asList(
+                CeylonTypes.BLOCK, CeylonTypes.CLASS_BODY, CeylonTypes.INTERFACE_BODY
+        ).contains(node.getElementType()) ? "{...}" : "...";
     }
 
     @Override
