9b64bd8449035146c0b7969c724f276ce4fd9ffa
gavinking <gavin@ceylon-lang.org>
2016-06-21 14:04:10 +0200

    some misc work on the formatter

--- src/org/intellij/plugins/ceylon/ide/formatting/CeylonBlock.java
+++ src/org/intellij/plugins/ceylon/ide/formatting/CeylonBlock.java
@@ -19,6 +19,7 @@ import java.util.Collection;
 import java.util.List;
 
 class CeylonBlock implements Block {
+
     private final ASTNode node;
     private final Indent indent;
 
@@ -33,6 +34,7 @@ class CeylonBlock implements Block {
     private static final Spacing SINGLE_SPACE_SPACING = Spacing.createSpacing(1, 1, 0, true, 0);
     private static final Spacing SINGLE_SPACE_SPACING_INLINE = Spacing.createSpacing(1, 1, 0, false, 0);
     private static final Spacing KEEP_SOME_SPACE = Spacing.createSpacing(1, 1, 0, true, 1);
+    private static final Spacing COMMENT_SPACING = Spacing.createSpacing(0, 100, 0, true, 1);
 
     private static final List<IElementType> SMALLER_OP = Arrays.asList(CeylonTypes.SMALLER_OP, CeylonTokens.SMALLER_OP);
     private static final List<IElementType> LARGER_OP = Arrays.asList(CeylonTypes.LARGER_OP, CeylonTokens.LARGER_OP);
@@ -51,36 +53,58 @@ class CeylonBlock implements Block {
             CeylonTypes.EXTENDED_TYPE, CeylonTypes.SATISFIED_TYPES, CeylonTypes.CASE_TYPES,
             CeylonTypes.LAZY_SPECIFIER_EXPRESSION, CeylonTypes.SPECIFIER_EXPRESSION
     );
+    public static final List<IElementType> MEMBER_OPS = Arrays.asList(
+            CeylonTypes.MEMBER_OP, CeylonTokens.MEMBER_OP, CeylonTypes.SAFE_MEMBER_OP
+    );
+    public static final List<IElementType> TYPES_REQUIRING_NO_SPACING = Arrays.asList(
+            CeylonTypes.TYPE_ARGUMENT_LIST, CeylonTypes.TYPE_PARAMETER_LIST,
+            CeylonTypes.SEQUENCED_TYPE, CeylonTypes.INDEX_EXPRESSION
+    );
 
+    //be very careful here not to mix up CeylonTokens vs CeylonTypes!
     private static final Collection<IElementType> TYPES_REQUIRING_NO_LEFT_SPACING = Arrays.asList(
-            CeylonTypes.PARAMETER_LIST, CeylonTokens.RPAREN, CeylonTokens.COMMA, CeylonTokens.SEMICOLON,
-            CeylonTypes.TYPE_PARAMETER_LIST, CeylonTypes.INVOCATION_EXPRESSION,
-            CeylonTokens.UNION_OP, CeylonTypes.UNION_OP, CeylonTokens.INTERSECTION_OP, CeylonTypes.INTERSECTION_OP,
-            CeylonTypes.TYPE_ARGUMENT_LIST,
-            CeylonTypes.SEQUENCED_TYPE, CeylonTokens.OPTIONAL, CeylonTokens.RBRACKET,
-            CeylonTypes.INCREMENT_OP, CeylonTypes.DECREMENT_OP, CeylonTypes.POSTFIX_INCREMENT_OP, CeylonTypes.POSTFIX_DECREMENT_OP,
-            CeylonTokens.LBRACKET,
+            CeylonTypes.PARAMETER_LIST, CeylonTypes.TYPE_PARAMETER_LIST,
+            CeylonTypes.INVOCATION_EXPRESSION, CeylonTypes.TYPE_ARGUMENT_LIST,
+            CeylonTypes.POSTFIX_INCREMENT_OP, CeylonTypes.POSTFIX_DECREMENT_OP,
+            CeylonTypes.INCREMENT_OP, CeylonTypes.DECREMENT_OP,
+            CeylonTypes.SEQUENCED_TYPE,
+            CeylonTokens.RPAREN, CeylonTokens.RBRACKET, CeylonTokens.LBRACKET,
+            CeylonTokens.COMMA, CeylonTokens.SEMICOLON,
+            CeylonTokens.UNION_OP, CeylonTypes.UNION_OP,
+            CeylonTokens.INTERSECTION_OP, CeylonTypes.INTERSECTION_OP,
+            CeylonTokens.OPTIONAL,
             CeylonTokens.ENTRY_OP, CeylonTypes.ENTRY_OP,
             CeylonTokens.SMALL_AS_OP, CeylonTokens.SMALLER_OP,
             CeylonTokens.LARGE_AS_OP, CeylonTokens.LARGER_OP
     );
+    //be very careful here not to mix up CeylonTokens vs CeylonTypes!
     private static final Collection<IElementType> TYPES_REQUIRING_NO_RIGHT_SPACING = Arrays.asList(
-            CeylonTokens.LPAREN,
-            CeylonTypes.MEMBER_OP, CeylonTokens.MEMBER_OP,
-            CeylonTokens.UNION_OP, CeylonTypes.UNION_OP, CeylonTokens.INTERSECTION_OP, CeylonTypes.INTERSECTION_OP,
-            CeylonTypes.NOT_OP, CeylonTypes.TYPE_ARGUMENT_LIST,
+            CeylonTypes.TYPE_ARGUMENT_LIST,
             CeylonTypes.INCREMENT_OP, CeylonTypes.DECREMENT_OP,
-            CeylonTokens.LBRACKET, CeylonTypes.SAFE_MEMBER_OP,
-            CeylonTypes.SPREAD_OP,
-            CeylonTokens.ENTRY_OP, CeylonTypes.ENTRY_OP,
             CeylonTypes.POSITIVE_OP, CeylonTypes.NEGATIVE_OP,
+            CeylonTokens.LPAREN,
+            CeylonTokens.MEMBER_OP, CeylonTypes.MEMBER_OP,
+            CeylonTokens.UNION_OP, CeylonTypes.UNION_OP,
+            CeylonTokens.INTERSECTION_OP, CeylonTypes.INTERSECTION_OP,
+            CeylonTokens.NOT_OP, CeylonTypes.NOT_OP,
+            CeylonTokens.LBRACKET, CeylonTypes.SAFE_MEMBER_OP,
+            CeylonTokens.SPREAD_OP, CeylonTypes.SPREAD_OP,
+            CeylonTokens.ENTRY_OP, CeylonTypes.ENTRY_OP,
             CeylonTokens.SMALL_AS_OP, CeylonTokens.SMALLER_OP,
             CeylonTokens.LARGE_AS_OP, CeylonTokens.LARGER_OP
     );
     private static final Collection<IElementType> TYPES_REQUIRING_EMPTY_LINE = Arrays.asList(
             CeylonTypes.IMPORT_LIST, CeylonTypes.CLASS_DEFINITION, CeylonTypes.METHOD_DEFINITION
     );
-    private static final Collection<IElementType> COMMENTS = Arrays.asList(CeylonTokens.LINE_COMMENT, CeylonTokens.MULTI_COMMENT);
+    public static final List<IElementType> CLASS_OR_INTERFACE = Arrays.asList(
+            CeylonTypes.CLASS_DEFINITION, CeylonTypes.INTERFACE_DEFINITION
+    );
+    public static final List<IElementType> INLINE_KEYWORDS = Arrays.asList(
+            CeylonTypes.IMPORT_MEMBER, CeylonTypes.IMPORT,
+            CeylonTypes.THROW, CeylonTypes.RETURN, CeylonTypes.ASSERTION,
+            CeylonTypes.SATISFIED_TYPES, CeylonTypes.EXTENDED_TYPE, CeylonTypes.CASE_TYPES
+    );
+
 
     CeylonBlock(ASTNode node, Indent indent) {
 //        System.out.printf("Indent for %s: %s%n", node.getElementType(), indent);
@@ -100,31 +124,38 @@ class CeylonBlock implements Block {
         List<Block> blocks = new ArrayList<>();
 
         final IElementType nodeType = node.getElementType();
-        final Indent normalChildIndent = INDENT_CHILDREN_NORMAL.contains(nodeType) ? Indent.getNormalIndent()
+        final Indent normalChildIndent =
+                  INDENT_CHILDREN_NORMAL.contains(nodeType) ? Indent.getNormalIndent()
                 : INDENT_CHILDREN_NONE.contains(nodeType) ? Indent.getNoneIndent()
                 : INDENT_CHILDREN_CONTINUE.contains(nodeType) ? Indent.getContinuationIndent()
                 : Indent.getNoneIndent();
 //        System.out.printf("normalChildIndent for %s: %s%n", nodeType, indent);
         IElementType prevChildType = null;
         for (ASTNode child = node.getFirstChildNode(); child != null; child = child.getTreeNext()) {
-            if (!FormatterUtil.containsWhiteSpacesOnly(child) && child.getTextLength() > 0) {
+            IElementType type = child.getElementType();
+            if (!FormatterUtil.containsWhiteSpacesOnly(child)
+                    && child.getTextLength() > 0) {
                 Indent indent = normalChildIndent;
-                if (child.getElementType() == CeylonTokens.RBRACE || child.getElementType() == CeylonTokens.LBRACE) {
+                if (type == CeylonTokens.RBRACE
+                 || type == CeylonTokens.LBRACE) {
                     indent = Indent.getNoneIndent();
                 }
                 if (prevChildType == CeylonTypes.ANNOTATION_LIST && indent == null) {
                     indent = Indent.getNoneIndent();
                 }
-                if (child.getElementType() == CeylonTokens.MEMBER_OP) {
+                if (type == CeylonTokens.MEMBER_OP) {
                     indent = Indent.getNormalIndent();
                 }
-                if (child.getElementType() == CeylonTypes.LISTED_ARGUMENT) {
+                if (type == CeylonTypes.LISTED_ARGUMENT) {
                     indent = Indent.getNormalIndent();
                 }
+
                 blocks.add(new CeylonBlock(child, indent));
             }
-            if (child.getElementType() != TokenType.WHITE_SPACE) {
-                prevChildType = child.getElementType();
+            if (type != TokenType.WHITE_SPACE
+                    && type != CeylonTokens.LINE_COMMENT
+                    && type != CeylonTokens.MULTI_COMMENT) {
+                prevChildType = type;
             }
         }
 
@@ -164,21 +195,26 @@ class CeylonBlock implements Block {
 
         Spacing result;
 
-        if (block1 == null || COMMENTS.contains(type1)) {
-            result = NO_SPACING;
-        } else if (COMMENTS.contains(type2)) {
-            result = SINGLE_SPACE_SPACING_INLINE;
+        if (block1 == null) {
+            return NO_SPACING;
+        }
+        else if (type1 == CeylonTokens.MULTI_COMMENT
+              || type2 == CeylonTokens.MULTI_COMMENT
+              || type2 == CeylonTokens.LINE_COMMENT
+              || type1 == CeylonTokens.LINE_COMMENT) {
+            result = COMMENT_SPACING; //or just return null?
         } else if (TYPES_REQUIRING_EMPTY_LINE.contains(type1) && type2 != CeylonTokens.RBRACE
-                || TYPES_REQUIRING_EMPTY_LINE.contains(type2) && !Arrays.asList(CeylonTokens.LBRACE, CeylonTypes.ANNOTATION_LIST).contains(type1)) {
+                || TYPES_REQUIRING_EMPTY_LINE.contains(type2) && type1 != CeylonTokens.LBRACE && CeylonTypes.ANNOTATION_LIST != type1) {
             result = EMPTY_LINE_SPACING;
         } else if (CeylonTypes.IMPORT_MODULE.equals(type1)) {
             result = NEW_LINE_SPACING;
         } else if (CeylonTypes.IMPORT_MEMBER.equals(type2)) {
             result = NEW_LINE_SPACING;
         } else if ((type1 == CeylonTokens.UNION_OP || type2 == CeylonTokens.UNION_OP)
-                && nodeType == CeylonTypes.CASE_TYPES){
+                && nodeType == CeylonTypes.CASE_TYPES) {
             result = SINGLE_SPACE_SPACING;
-        } else if (TYPES_REQUIRING_NO_RIGHT_SPACING.contains(type1) || TYPES_REQUIRING_NO_LEFT_SPACING.contains(type2)
+        } else if (TYPES_REQUIRING_NO_RIGHT_SPACING.contains(type1)
+                || TYPES_REQUIRING_NO_LEFT_SPACING.contains(type2)
                 || (type1 == CeylonTypes.SMALLER_OP && type2 == CeylonTypes.TYPE_PARAMETER_DECLARATION)
                 || (type1 == CeylonTypes.TYPE_PARAMETER_LITERAL && type2 == CeylonTypes.LARGER_OP)
                 || (type2 == CeylonTypes.POSITIONAL_ARGUMENT_LIST && nodeType != CeylonTypes.ANNOTATION)
@@ -187,9 +223,9 @@ class CeylonBlock implements Block {
         } else if ((SMALLER_OP.contains(type1) && isType(block2))
                 || (LARGER_OP.contains(type2) && isType(block1))) {
             result = NO_SPACING;
-        } else if (Arrays.asList(CeylonTypes.MEMBER_OP, CeylonTokens.MEMBER_OP, CeylonTypes.SAFE_MEMBER_OP).contains(type2)) {
+        } else if (MEMBER_OPS.contains(type2)) {
             result = nodeType == CeylonTypes.QUALIFIED_MEMBER_EXPRESSION ? NO_SPACE_ALLOW_NEWLINE : NO_SPACING;
-        } else if (Arrays.asList(CeylonTypes.TYPE_ARGUMENT_LIST, CeylonTypes.TYPE_PARAMETER_LIST, CeylonTypes.SEQUENCED_TYPE, CeylonTypes.INDEX_EXPRESSION).contains(nodeType)) {
+        } else if (TYPES_REQUIRING_NO_SPACING.contains(nodeType)) {
             result = NO_SPACING;
         } else if (type1 == CeylonTokens.LBRACE && type2 == CeylonTokens.RBRACE) {
             result = NO_SPACING;
@@ -199,13 +235,13 @@ class CeylonBlock implements Block {
             result = CeylonTokens.COMMA.equals(type1) ? SINGLE_SPACE_SPACING : NO_SPACING;
         } else if (type1 == CeylonTypes.ANNOTATION_LIST) {
             result = KEEP_SOME_SPACE;
-        } else if (Arrays.asList(CeylonTypes.IMPORT_MEMBER, CeylonTypes.THROW, CeylonTypes.IMPORT, CeylonTypes.ASSERTION, CeylonTypes.SATISFIED_TYPES, CeylonTypes.EXTENDED_TYPE).contains(nodeType)) {
+        } else if (INLINE_KEYWORDS.contains(nodeType)) {
             result = SINGLE_SPACE_SPACING_INLINE;
         } else if (nodeType == CeylonTypes.ANNOTATION_LIST && type1 == CeylonTypes.STRING_LITERAL) {
             result = NEW_LINE_SPACING;
         } else if (CeylonTypes.METHOD_DEFINITION.equals(nodeType) && type2 == CeylonTypes.BLOCK) {
             result = SINGLE_SPACE_SPACING_INLINE;
-        } else if (bothTypes.contains(CeylonTypes.IDENTIFIER) && Arrays.asList(CeylonTypes.CLASS_DEFINITION, CeylonTypes.INTERFACE_DEFINITION).contains(nodeType)) {
+        } else if (bothTypes.contains(CeylonTypes.IDENTIFIER) && CLASS_OR_INTERFACE.contains(nodeType)) {
             result = SINGLE_SPACE_SPACING_INLINE;
         } else if (INDENT_CHILDREN_NORMAL.contains(nodeType)
                 && (type2 == CeylonTokens.RBRACE && type1 != CeylonTokens.LBRACE)) {
