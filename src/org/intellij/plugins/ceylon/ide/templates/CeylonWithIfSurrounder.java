f1db7764a7ad8021ac948b8b5dd4d6b32ffbb3f9
gavinking <gavin@ceylon-lang.org>
2016-07-04 10:25:04 +0200

    Revert "surround with 'if'"
    This reverts commit 4eaeab5af4d13ef7be340cdc57d6be854acf8430.

--- src/org/intellij/plugins/ceylon/ide/templates/CeylonWithIfSurrounder.java
+++ src/org/intellij/plugins/ceylon/ide/templates/CeylonWithIfSurrounder.java
@@ -1,31 +1,25 @@
 package org.intellij.plugins.ceylon.ide.templates;
 
 import com.intellij.codeInsight.CodeInsightUtilCore;
-import com.intellij.lang.surroundWith.SurroundDescriptor;
 import com.intellij.lang.surroundWith.Surrounder;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.project.Project;
-import com.intellij.openapi.util.Condition;
 import com.intellij.openapi.util.TextRange;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiFile;
 import com.intellij.psi.PsiFileFactory;
-import com.intellij.psi.PsiWhiteSpace;
 import com.intellij.psi.util.PsiTreeUtil;
+import com.intellij.psi.util.PsiUtilCore;
 import com.intellij.util.IncorrectOperationException;
 import org.intellij.plugins.ceylon.ide.ceylonCode.lang.CeylonFileType;
-import org.intellij.plugins.ceylon.ide.ceylonCode.psi.CeylonCompositeElement;
 import org.intellij.plugins.ceylon.ide.ceylonCode.psi.CeylonPsi;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.ArrayList;
-import java.util.List;
-
-public class CeylonWithIfSurrounder implements Surrounder, SurroundDescriptor {
+class CeylonWithIfSurrounder implements Surrounder {
     @Override
     public String getTemplateDescription() {
-        return "'if' statement";
+        return "do you see me?";
     }
 
     @Override
@@ -35,100 +29,26 @@ public class CeylonWithIfSurrounder implements Surrounder, SurroundDescriptor {
 
     @Nullable
     @Override
-    public TextRange surroundElements(@NotNull Project project, @NotNull Editor editor, @NotNull PsiElement[] elements)
-            throws IncorrectOperationException {
+    public TextRange surroundElements(@NotNull Project project, @NotNull Editor editor, @NotNull PsiElement[] elements) throws IncorrectOperationException {
+        String content = "void a(){if(true){\n}}";
+        PsiFile file = PsiFileFactory.getInstance(project).createFileFromText("dummy.ceylon", CeylonFileType.INSTANCE, content);
 
-        String content = "void a(){if(true){throw;}}";
+        CeylonPsi.BooleanConditionPsi condition = PsiTreeUtil.getParentOfType(PsiUtilCore.getElementAtOffset(file, 12), CeylonPsi.BooleanConditionPsi.class);
+        CeylonPsi.IfStatementPsi ifStatement = PsiTreeUtil.getParentOfType(condition, CeylonPsi.IfStatementPsi.class);
 
-        PsiFile file
-                = PsiFileFactory.getInstance(project)
-                    .createFileFromText("dummy.ceylon",
-                            CeylonFileType.INSTANCE,
-                            content);
+        assert condition != null;
+        assert ifStatement != null;
 
-        CeylonPsi.IfStatementPsi ifStatement
-                = PsiTreeUtil.findElementOfClassAtOffset(file, 9,
-                    CeylonPsi.IfStatementPsi.class, true);
+        condition.replace(elements[0]);
 
+        ifStatement = (CeylonPsi.IfStatementPsi) elements[0].getParent().replace(ifStatement);
 
-        CeylonPsi.BlockPsi block
-                = PsiTreeUtil.findElementOfClassAtOffset(file, 17,
-                    CeylonPsi.BlockPsi.class, true);
-
-        CeylonPsi.ThrowPsi throwStatement
-                = PsiTreeUtil.findElementOfClassAtOffset(file, 18,
-                    CeylonPsi.ThrowPsi.class, true);
-
-        PsiElement parent = elements[0].getParent();
-
-        for (PsiElement element: elements) {
-            block.addBefore(element, throwStatement);
-        }
-        throwStatement.delete();
-
-        PsiElement result = parent.addBefore(ifStatement, elements[0]);
-
-        for (PsiElement element: elements) {
-            element.delete();
+        CeylonPsi.BlockPsi block = PsiTreeUtil.getChildOfType(ifStatement.getChildren()[0], CeylonPsi.BlockPsi.class);
+        if (block != null) {
+            block = CodeInsightUtilCore.forcePsiPostprocessAndRestoreElement(block);
+            return TextRange.from(block.getTextOffset() + 1, 0);
         }
 
-        result = CodeInsightUtilCore.forcePsiPostprocessAndRestoreElement(result);
-
-        int loc = result.getTextOffset() + result.getText().indexOf("true");
-        return new TextRange(loc, loc+4);
-    }
-
-    private Condition<PsiElement> condition = new Condition<PsiElement>() {
-        @Override
-        public boolean value(PsiElement element) {
-            return element instanceof CeylonPsi.StatementOrArgumentPsi;
-        }
-    };
-
-    @NotNull
-    @Override
-    public PsiElement[] getElementsToSurround(PsiFile file, int selectionStart, int selectionEnd) {
-        PsiElement start = file.findElementAt(selectionStart);
-        PsiElement end = file.findElementAt(selectionEnd);
-        if (start instanceof PsiWhiteSpace) {
-            start = PsiTreeUtil.getNextSiblingOfType(start,
-                    CeylonCompositeElement.class);
-        }
-        if (end instanceof PsiWhiteSpace) {
-            end = PsiTreeUtil.getPrevSiblingOfType(end,
-                    CeylonCompositeElement.class);
-        }
-
-        CeylonPsi.StatementOrArgumentPsi first
-                = (CeylonPsi.StatementOrArgumentPsi)
-                PsiTreeUtil.findFirstParent(start, condition);
-        CeylonPsi.StatementOrArgumentPsi last
-                = (CeylonPsi.StatementOrArgumentPsi)
-                PsiTreeUtil.findFirstParent(end, condition);
-        if (first==null || last==null) {
-            return PsiElement.EMPTY_ARRAY;
-        }
-
-        List<PsiElement> list = new ArrayList<>();
-        list.add(first);
-        CeylonPsi.StatementOrArgumentPsi current = first;
-        while (current!=last && current!=null) {
-            current = PsiTreeUtil.getNextSiblingOfType(current,
-                    CeylonPsi.StatementOrArgumentPsi.class);
-            list.add(current);
-        }
-
-        return list.toArray(PsiElement.EMPTY_ARRAY);
-    }
-
-    @NotNull
-    @Override
-    public Surrounder[] getSurrounders() {
-        return new Surrounder[] {this};
-    }
-
-    @Override
-    public boolean isExclusive() {
-        return false;
+        return null;
     }
 }
