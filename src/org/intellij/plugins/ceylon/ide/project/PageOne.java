223d3bb51784fc2c1fed5a8b5ea211c180e377fc
gavinking <gavin@ceylon-lang.org>
2016-09-29 12:45:05 +0200

    suppress warnings at project level #207

--- src/org/intellij/plugins/ceylon/ide/project/PageOne.java
+++ src/org/intellij/plugins/ceylon/ide/project/PageOne.java
@@ -1,15 +1,22 @@
 package org.intellij.plugins.ceylon.ide.project;
 
+import com.redhat.ceylon.compiler.typechecker.analyzer.Warning;
 import org.intellij.plugins.ceylon.ide.ceylonCode.model.IdeaCeylonProject;
 import org.intellij.plugins.ceylon.ide.ceylonCode.settings.ceylonSettings_;
+import org.jetbrains.annotations.NotNull;
 
 import javax.swing.*;
+import java.util.ArrayList;
+import java.util.EnumSet;
+import java.util.List;
+import java.util.StringTokenizer;
 
 public class PageOne implements CeylonConfigForm {
     private JCheckBox compileForJvm;
     private JCheckBox compileToJs;
     private JPanel panel;
     private JCheckBox workOffline;
+    private JTextArea suppressedWarnings;
 
     public PageOne() {
         String defaultVm = ceylonSettings_.get_().getDefaultTargetVm();
@@ -27,6 +34,7 @@ public class PageOne implements CeylonConfigForm {
         project.getIdeConfiguration().setCompileToJvm(ceylon.language.Boolean.instance(compileForJvm.isSelected()));
         project.getIdeConfiguration().setCompileToJs(ceylon.language.Boolean.instance(compileToJs.isSelected()));
         project.getConfiguration().setProjectOffline(ceylon.language.Boolean.instance(workOffline.isSelected()));
+        project.getConfiguration().setProjectSuppressWarningsEnum(EnumSet.copyOf(parseWarnings(suppressedWarnings.getText())));
     }
 
     @Override
@@ -36,7 +44,8 @@ public class PageOne implements CeylonConfigForm {
                 || project.getConfiguration().getProjectOffline() == null
                 || project.getIdeConfiguration().getCompileToJvm().booleanValue() != compileForJvm.isSelected()
                 || project.getIdeConfiguration().getCompileToJs().booleanValue() != compileToJs.isSelected()
-                || project.getConfiguration().getProjectOffline().booleanValue() != workOffline.isSelected();
+                || project.getConfiguration().getProjectOffline().booleanValue() != workOffline.isSelected()
+                || warningsAsString(project.getConfiguration().getSuppressWarningsEnum()) != suppressedWarnings.getText();
     }
 
     @Override
@@ -45,6 +54,32 @@ public class PageOne implements CeylonConfigForm {
         compileForJvm.setSelected(safeNullBoolean(project.getIdeConfiguration().getCompileToJvm(), !defaultVm.equals("js")));
         compileToJs.setSelected(safeNullBoolean(project.getIdeConfiguration().getCompileToJs(), !defaultVm.equals("jvm")));
         workOffline.setSelected(safeNullBoolean(project.getConfiguration().getProjectOffline(), false));
+        suppressedWarnings.setText(warningsAsString(project.getConfiguration().getSuppressWarningsEnum()));
+    }
+
+    @NotNull
+    private List<Warning> parseWarnings(String text) {
+        List<Warning> warnings = new ArrayList<Warning>();
+        StringTokenizer tokenizer = new StringTokenizer(text, ",; \t\n\r\f");
+        while (tokenizer.hasMoreTokens()) {
+            try {
+                warnings.add(Warning.valueOf(tokenizer.nextToken()));
+            }
+            catch (IllegalArgumentException iae) {}
+        }
+        return warnings;
+    }
+
+    @NotNull
+    private String warningsAsString(Iterable<Warning> warnings) {
+        StringBuilder text = new StringBuilder();
+        for (Warning names: warnings) {
+            if (text.length()>0) {
+                text.append(", ");
+            }
+            text.append(names);
+        }
+        return text.toString();
     }
 
     private boolean safeNullBoolean(ceylon.language.Boolean bool, boolean def) {
