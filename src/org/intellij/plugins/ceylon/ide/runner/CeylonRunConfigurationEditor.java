2ac70678432f112d854d37b60b95708c45e3fff4
Bastien Jansen <bastien.jansen@gmx.com>
2016-10-05 18:28:15 +0200

    Choosers for module name and runnable name in Run configurations (pre-#515).

--- src/org/intellij/plugins/ceylon/ide/runner/CeylonRunConfigurationEditor.java
+++ src/org/intellij/plugins/ceylon/ide/runner/CeylonRunConfigurationEditor.java
@@ -1,33 +1,46 @@
 package org.intellij.plugins.ceylon.ide.runner;
 
+import ceylon.interop.java.JavaIterable;
+import ceylon.language.Iterable;
 import com.intellij.application.options.ModulesComboBox;
 import com.intellij.openapi.options.ConfigurationException;
 import com.intellij.openapi.options.SettingsEditor;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.ui.TextFieldWithBrowseButton;
 import com.intellij.ui.RawCommandLineEditor;
+import com.redhat.ceylon.ide.common.model.CeylonProject;
+import com.redhat.ceylon.model.typechecker.model.Declaration;
+import com.redhat.ceylon.model.typechecker.model.Module;
+import org.intellij.plugins.ceylon.ide.ceylonCode.model.IdeaCeylonProject;
+import org.intellij.plugins.ceylon.ide.ceylonCode.model.IdeaCeylonProjects;
+import org.intellij.plugins.ceylon.ide.ceylonCode.model.IdeaModule;
 import org.jetbrains.annotations.NotNull;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import static com.redhat.ceylon.compiler.java.runtime.model.TypeDescriptor.klass;
+import static org.intellij.plugins.ceylon.ide.ceylonCode.model.getCeylonProjects_.getCeylonProjects;
 
 /**
  * Graphical editor for Ceylon run configurations.
  */
 public class CeylonRunConfigurationEditor extends SettingsEditor<CeylonRunConfiguration> {
-    private JTextField myRunnableName;
-    private JTextField myCeylonModule;
+    private TextFieldWithBrowseButton myRunnableName;
+    private TextFieldWithBrowseButton myCeylonModule;
     private JPanel myPanel;
     private RawCommandLineEditor myArguments;
     private RawCommandLineEditor myVmOptions;
     private ModulesComboBox myIdeModule;
+    private Project project;
 
-    public CeylonRunConfigurationEditor(Project project) {
-//        myFileName.addBrowseFolderListener("Choose .ceylon file", "Please choose the path to the Ceylon file to run",
-//                project, FileChooserDescriptorFactory.createSingleLocalFileDescriptor());
-//        myPackage = new PackageNameReferenceEditorCombo(null, project, null, RefactoringBundle.message("choose.destination.package"));
+    CeylonRunConfigurationEditor(final Project project) {
+        this.project = project;
     }
 
     @Override
-    protected void resetEditorFrom(CeylonRunConfiguration config) {
+    protected void resetEditorFrom(@NotNull CeylonRunConfiguration config) {
         myRunnableName.setText(config.getTopLevelNameFull());
         myCeylonModule.setText(config.getCeylonModule());
         myArguments.setText(config.getArguments());
@@ -37,12 +50,12 @@ public class CeylonRunConfigurationEditor extends SettingsEditor<CeylonRunConfig
     }
 
     @Override
-    protected void applyEditorTo(CeylonRunConfiguration s) throws ConfigurationException {
-        s.setTopLevelNameFull(myRunnableName.getText());
-        s.setCeylonModule(myCeylonModule.getText());
-        s.setArguments(myArguments.getText());
-        s.setVmOptions(myVmOptions.getText());
-        s.getConfigurationModule().setModule(myIdeModule.getSelectedModule());
+    protected void applyEditorTo(@NotNull CeylonRunConfiguration config) throws ConfigurationException {
+        config.setTopLevelNameFull(myRunnableName.getText());
+        config.setCeylonModule(myCeylonModule.getText());
+        config.setArguments(myArguments.getText());
+        config.setVmOptions(myVmOptions.getText());
+        config.getConfigurationModule().setModule(myIdeModule.getSelectedModule());
     }
 
     @NotNull
@@ -52,6 +65,51 @@ public class CeylonRunConfigurationEditor extends SettingsEditor<CeylonRunConfig
     }
 
     private void createUIComponents() {
+        myCeylonModule = new TextFieldWithBrowseButton(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                ModuleChooserDialog dialog = new ModuleChooserDialog(project);
+                dialog.show();
+                if (dialog.isOK()) {
+                    Module module = dialog.getSelectedModule();
+                    if (module != null) {
+                        myCeylonModule.setText(module.getNameAsString());
+                        Object ideArtifact = ((IdeaModule) module).getCeylonProject().getIdeArtifact();
+                        myIdeModule.setSelectedModule((com.intellij.openapi.module.Module) ideArtifact);
+                    }
+                }
+            }
+        });
+
+        myRunnableName = new TextFieldWithBrowseButton(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (myCeylonModule.getText() != null) {
+                    IdeaCeylonProjects projects = getCeylonProjects(project);
+                    if (projects != null) {
+                        for (CeylonProject p : new JavaIterable<>(klass(IdeaCeylonProject.class), projects.getCeylonProjects())) {
+                            Iterable modules = p.getModules().getFromProject();
+                            for (int i = 0; i < modules.getSize(); i++) {
+                                IdeaModule mod = (IdeaModule) modules.getFromFirst(i);
+
+                                if (mod.getNameAsString().equals(myCeylonModule.getText())) {
+                                    RunnableChooserDialog dialog = new RunnableChooserDialog(project, mod);
+                                    dialog.show();
+                                    if (dialog.isOK()) {
+                                        Declaration decl = dialog.getSelectedDeclaration();
+                                        if (decl != null) {
+                                            myRunnableName.setText(decl.getQualifiedNameString());
+                                        }
+                                    }
+
+                                    return;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        });
     }
 
 }
---
+++ src/org/intellij/plugins/ceylon/ide/runner/RunnableChooserDialog.java
@@ -0,0 +1,121 @@
+package org.intellij.plugins.ceylon.ide.runner;
+
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.ui.DialogWrapper;
+import com.redhat.ceylon.model.typechecker.model.Class;
+import com.redhat.ceylon.model.typechecker.model.*;
+import com.redhat.ceylon.model.typechecker.model.Package;
+import org.intellij.plugins.ceylon.ide.ceylonCode.highlighting.highlighter_;
+import org.intellij.plugins.ceylon.ide.ceylonCode.model.IdeaModule;
+import org.intellij.plugins.ceylon.ide.ceylonCode.util.icons_;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import javax.swing.*;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+public class RunnableChooserDialog extends DialogWrapper {
+    private final IdeaModule module;
+    private JPanel contentPane;
+    private JList<Declaration> runnableList;
+
+    RunnableChooserDialog(@Nullable final Project project, IdeaModule module) {
+        super(project, false);
+        init();
+        setTitle("Choose a Runnable Declaration");
+
+        this.module = module;
+
+        runnableList.setModel(createModel());
+        runnableList.setCellRenderer(new DefaultListCellRenderer() {
+            @Override
+            public Component getListCellRendererComponent(JList<?> list, Object value, int index,
+                                                          boolean isSelected, boolean cellHasFocus) {
+                Component cmp = super.getListCellRendererComponent(list, value, index,
+                        isSelected, cellHasFocus);
+                if (value instanceof Declaration) {
+                    setIcon(icons_.get_().forDeclaration(value));
+                    setText("<html>" + highlighter_.get_().highlight(getText(), project) + "</html>");
+                }
+                return cmp;
+            }
+        });
+        setOKActionEnabled(false);
+        runnableList.addListSelectionListener(new ListSelectionListener() {
+            @Override
+            public void valueChanged(ListSelectionEvent e) {
+                setOKActionEnabled(runnableList.getSelectedValue() != null);
+            }
+        });
+        runnableList.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                if (e.getClickCount() == 2) {
+                    Rectangle r = runnableList.getCellBounds(0, runnableList.getLastVisibleIndex());
+                    if (r != null && r.contains(e.getPoint())) {
+                        int index = runnableList.locationToIndex(e.getPoint());
+                        if (index != -1) {
+                            doOKAction();
+                        }
+                    }
+                }
+            }
+        });
+    }
+
+    @Nullable
+    Declaration getSelectedDeclaration() {
+        return runnableList.getSelectedValue();
+    }
+
+    @NotNull
+    private DefaultListModel<Declaration> createModel() {
+        DefaultListModel<Declaration> model = new DefaultListModel<>();
+
+        for (Package pkg : module.getPackages()) {
+            for (Declaration declaration : pkg.getMembers()) {
+                if (declaration.isToplevel()
+                        && declaration.isShared()
+                        && isRunnable(declaration)) {
+                    model.addElement(declaration);
+                }
+            }
+        }
+
+        return model;
+    }
+
+    private boolean isRunnable(Declaration decl) {
+        if (decl instanceof Class) {
+            Class cls = (Class) decl;
+            if (cls.isObjectClass()) {
+                return false;
+            }
+            ParameterList plist = cls.getParameterList();
+            return plist == null || plist.getParameters().size() == 0;
+        } else if (decl instanceof Function) {
+            Function fun = (Function) decl;
+            if (fun.getParameterLists().size() == 1) {
+                return fun.getFirstParameterList().getParameters().size() == 0;
+            }
+        }
+
+        return false;
+    }
+
+    @Nullable
+    @Override
+    public JComponent getPreferredFocusedComponent() {
+        return runnableList;
+    }
+
+    @Nullable
+    @Override
+    protected JComponent createCenterPanel() {
+        return contentPane;
+    }
+}
